6a490cddae1826bde08bfa90445bd2110abe775c
Regina Casias <reginaannvigil@gmail.com>
2021-09-05 22:49:24 -0600

    Added testing for finding user by id

--- spec/graphql/queries/user_spec.rb
+++ spec/graphql/queries/user_spec.rb
@@ -1,49 +1,66 @@
 require 'rails_helper'
 
-RSpec.describe 'create_user', type: :request do
-  # describe 'mutation: userCreate' do
-  #   it 'creates a user' do
-  #     string = <<~GQL
-  #       mutation {
-  #         createUser(input: {
-  #           userName: "ChrisPBacon",
-  #           firstName: "Chris",
-  #           lastName: "Bacon",
-  #           phoneNumber: "123-123-1234",
-  #           email: "example@example.com",
-  #           birthday: "2013-07-16"
-  #         }) {
-  #           user {
-  #             id
-  #             userName
-  #             firstName
-  #             lastName
-  #             email
-  #             phoneNumber
-  #             birthday
-  #           }
-  #         }
-  #       }
-  #     GQL
-  #
-  #     post graphql_path, params: { query: string }
-  #     json_response = JSON.parse(@response.body, symbolize_names: true)
-  #     #
-  #     # expect(json_response).to have_key(:data)
-  #     # expect(json_response[:data]).to have_key(:createUser)
-  #     # expect(json_response[:data][:createUser]).to have_key(:user)
-  #     # expect(json_response[:data][:createUser][:user]).to have_key(:id)
-  #     # expect(json_response[:data][:createUser][:user]).to have_key(:userName)
-  #     # expect(json_response[:data][:createUser][:user]).to have_key(:firstName)
-  #     # expect(json_response[:data][:createUser][:user]).to have_key(:lastName)
-  #     # expect(json_response[:data][:createUser][:user]).to have_key(:email)
-  #     # expect(json_response[:data][:createUser][:user]).to have_key(:phoneNumber)
-  #     # expect(json_response[:data][:createUser][:user]).to have_key(:birthday)
-  #     #
-  #     # json_response[:data][:createUser][:user].each_value do |value|
-  #     #   expect(value.class).to eq(String)
-  #     end
-  #   end
+RSpec.describe 'users', type: :request do
+  describe 'query: users' do
+    it 'view all users' do
+      create_list(:user, 5)
+      string = <<~GQL
+      query {
+              users {
+                id
+                userName
+                firstName
+                lastName
+                email
+                phoneNumber
+                birthday
+              }
+            }
+      GQL
 
-  # end
+      post graphql_path, params: { query: string }
+      json_response = JSON.parse(@response.body, symbolize_names: true)
+      # binding.pry
+      expect(json_response).to have_key(:data)
+      expect(json_response[:data][:users]).to be_an Array
+      expect(json_response[:data][:users].count).to eq(5)
+      expect(json_response[:data][:users].first).to have_key(:id)
+      expect(json_response[:data][:users].first).to have_key(:userName)
+      expect(json_response[:data][:users].first).to have_key(:firstName)
+      expect(json_response[:data][:users].first).to have_key(:lastName)
+      expect(json_response[:data][:users].first).to have_key(:email)
+      expect(json_response[:data][:users].first).to have_key(:phoneNumber)
+      expect(json_response[:data][:users].first).to have_key(:birthday)
+    end
+
+    it 'can find user by id' do
+      user_id = create(:user).id
+      string = <<~GQL
+      query {
+              user (id: "#{user_id}") {
+                id
+                userName
+                firstName
+                lastName
+                email
+                phoneNumber
+                birthday
+              }
+            }
+      GQL
+
+      post graphql_path, params: { query: string }
+      json_response = JSON.parse(@response.body, symbolize_names: true)
+      # binding.pry
+      expect(json_response).to have_key(:data)
+      expect(json_response[:data][:user]).to have_key(:id)
+      expect(json_response[:data][:user]).to have_key(:userName)
+      expect(json_response[:data][:user]).to have_key(:firstName)
+      expect(json_response[:data][:user]).to have_key(:lastName)
+      expect(json_response[:data][:user]).to have_key(:email)
+      expect(json_response[:data][:user]).to have_key(:phoneNumber)
+      expect(json_response[:data][:user]).to have_key(:birthday)
+    end
+
+  end
 end
